9f6a9b546fc1dc78e09705ed49506d2008990c34
Vansy2005 <23724681.sy@student.iuh.edu.vn>
2025-10-08 14:41:46 +0700

    sửa dao

---
+++ src/main/java/com/example/pharmacy_project/dao/NhanVienDAO.java
@@ -0,0 +1,20 @@
+/*
+ * @ (#) NhanVienDAO         08 Oct 2025
+ *
+ *Copyright (c) 2025 IUH . All right reserved
+ */
+package com.example.pharmacy_project.dao;
+
+import com.example.pharmacy_project.entities.NhanVien;
+
+/*
+ * @description:  This class represents a bank with many bank accounts
+ * @author: Nguyen Van Sy
+ * @version: 1.0
+ * @created: 08 Oct 2025 14:36
+ */
+public class NhanVienDAO {
+    public NhanVienDAO() {
+    }
+
+}
