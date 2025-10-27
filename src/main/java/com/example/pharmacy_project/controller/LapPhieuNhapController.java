d2a73ceb129423850bc4de5d6ec770803cded472
Cusnnn <ndhung020405@gmail.com>
2025-10-27 12:06:31 +0700

    them man hinh nha cung cap (#26)

--- src/main/java/com/example/pharmacy_project/controller/LapPhieuNhapController.java
+++ src/main/java/com/example/pharmacy_project/controller/LapPhieuNhapController.java
@@ -5,8 +5,20 @@
  */
 package com.example.pharmacy_project.controller;
 
+import com.example.pharmacy_project.dao.KeThuocDAO;
+import com.example.pharmacy_project.dao.NhaCungCapDAO;
+import com.example.pharmacy_project.dao.NhanVienDAO;
+import com.example.pharmacy_project.dao.NhomThuocDAO;
+import com.example.pharmacy_project.entities.NhaCungCap;
+import com.example.pharmacy_project.entities.NhanVien;
+import com.example.pharmacy_project.entities.NhomThuoc;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.ComboBox;
+import javafx.util.StringConverter;
+
+import java.util.ArrayList;
 
 /*
  * @description:
@@ -15,5 +27,38 @@ import javafx.scene.Parent;
  * @created: 10/7/2025 12:44 PM
  */
 public class LapPhieuNhapController {
+    @FXML
+    private ComboBox<NhaCungCap> cbxNhaCungCap;
 
+    @FXML
+    private ComboBox<NhanVien> cbxNhanVien;
+
+    private final NhaCungCapDAO ncc = new NhaCungCapDAO();
+    private final NhanVienDAO nv = new NhanVienDAO();
+    public void initialize(){
+        loadcbxNhaCungCap();
+        loadcbxNhanVien();
+        cbxNhanVien.setConverter(new StringConverter<>() {
+            @Override public String toString(NhanVien nv) { return nv == null ? "" : nv.getTenNhanVien(); }
+            @Override public NhanVien fromString(String s) { return null; } // không dùng
+        });
+        cbxNhaCungCap.setConverter(new StringConverter<>() {
+            @Override public String toString(NhaCungCap ncc) { return ncc == null ? "" : ncc.getTenNhaCungCap(); }
+
+            @Override
+            public NhaCungCap fromString(String s) {
+                return null;
+            }
+        });
+    }
+
+    private void loadcbxNhanVien() {
+        ArrayList<NhanVien> dsNhanVien = nv.getAllTblNhanVien();
+        cbxNhanVien.getItems().setAll(dsNhanVien);
+    }
+
+    private void loadcbxNhaCungCap() {
+        ArrayList<NhaCungCap> dsNhaCungCap = ncc.getAllTblNhaCungCap();
+        cbxNhaCungCap.getItems().setAll(dsNhaCungCap);
+    }
 }
